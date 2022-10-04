5e72b733f43c830aff114e52d771892b847d341c
unknown <selmarionx@gmail.com>
2022-10-04 17:53:21 +0300

    Создали простые задачки

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿// S1.11 Составить программу вывода на экран «столбиком» четырех любых чисел
+
+Random rnd = new Random();
+int a = rnd.Next(0,255);
+int b = rnd.Next(0,255);
+int c = rnd.Next(0,255);
+int d = rnd.Next(0,255);
+Console.WriteLine($"{a}\n{b}\n{c}\n{d}");
---
+++ Task8/Program.cs
@@ -0,0 +1,8 @@
+﻿// S1.8. Составить программу вывода на экран в одну строку четырех любых чисел с одним пробелом между ними.
+
+Random rnd = new Random();
+int a = rnd.Next(0,255);
+int b = rnd.Next(0,255);
+int c = rnd.Next(0,255);
+int d = rnd.Next(0,255);
+Console.WriteLine($"{a} {b} {c} {d}");
